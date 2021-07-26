f1842d52c756640255779f7c66b6d0a8574f847d
Николай Сандрюхин <n.sandryukhin@i-sys.ru>
2021-07-26 10:52:28 +0400

    task3 поправил 3 задание

--- src/ru/isys/trainings/task3/unit3/Timer.java
+++ src/ru/isys/trainings/task3/unit3/Timer.java
@@ -7,16 +7,11 @@ public class Timer {
     public static void getSeconds() {
 
         LocalDateTime localTime = LocalDateTime.now();
-        LocalDateTime tomorrow = localTime.plusDays(1);
-        LocalDateTime tomorrowDate = tomorrow
-                .minusSeconds(tomorrow.getSecond())
-                .minusHours(tomorrow.getHour())
-                .minusMinutes(tomorrow.getMinute());
+        LocalDateTime tomorrow = LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(0,0,0));
 
-        Duration duration = Duration.between(localTime, tomorrowDate);
+        Duration duration = Duration.between(localTime, tomorrow);
 
         System.out.println(duration.getSeconds());
-
     }
 
 }
