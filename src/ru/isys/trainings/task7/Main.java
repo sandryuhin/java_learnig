74e30148066b7db616c324c28e41c5620de97716
Николай Сандрюхин <n.sandryukhin@i-sys.ru>
2021-08-23 23:46:24 +0400

    task7 unit2

--- src/ru/isys/trainings/task7/Main.java
+++ src/ru/isys/trainings/task7/Main.java
@@ -1,11 +1,13 @@
 package ru.isys.trainings.task7;
 
 import ru.isys.trainings.task7.unit1.LinkList;
+import ru.isys.trainings.task7.unit2.HashedMap;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        // unit1
         LinkList<String> linkList = new LinkList<>();
 
         linkList.add("test1");
@@ -15,6 +17,11 @@ public class Main {
         for (String llValue : linkList) {
             System.out.println(llValue);
         }
+
+        // unit2
+        HashedMap<String, String> stringHashedMap = new HashedMap<>();
+        stringHashedMap.put("lalala", "nanana");
+        stringHashedMap.put("lalala", "nanana");
 
     }
 
--- src/ru/isys/trainings/task7/unit2/HashedMap.java
+++ src/ru/isys/trainings/task7/unit2/HashedMap.java
@@ -4,9 +4,11 @@ import ru.isys.trainings.task7.unit1.LinkList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class HashedMap<K, V> {
 
+    private final int defaultBucketCapacity = 16;
     private final List<LinkList<MapItem<K, V>>> buckets;
 
     private static class MapItem<K, V> {
@@ -22,22 +24,38 @@ public class HashedMap<K, V> {
     }
 
     public HashedMap() {
-        this.buckets = new ArrayList<>(16);
+        this.buckets = new ArrayList<>(defaultBucketCapacity);
+
+        for (int index = 0; index < defaultBucketCapacity; index++) {
+            buckets.add(index, setDefaultValuesInBucket());
+        }
     }
 
     public HashedMap(int capacity) {
         this.buckets = new ArrayList<>(capacity);
     }
 
-    public void put(K key, V value) {
+    public void put(K key, V value) throws IndexOutOfBoundsException, NoSuchElementException {
         int hashOfKey = key.hashCode();
-        int keyOfBucket = hashOfKey % 16;
+        int keyOfBucket = Math.abs(hashOfKey % 16) - 1;
 
         MapItem<K, V> newItem = new MapItem<>(key, value);
 
         if (buckets.get(keyOfBucket) == null) {
             buckets.get(keyOfBucket).set(keyOfBucket, newItem);
+        } else {
+            buckets.get(keyOfBucket).add(keyOfBucket, newItem);
         }
+    }
+
+    private LinkList<MapItem<K, V>> setDefaultValuesInBucket() {
+        LinkList<MapItem<K, V>> defaultList = new LinkList<>();
+
+        for (int index = 0; index < defaultBucketCapacity; index++) {
+            defaultList.add(null);
+        }
+
+        return defaultList;
     }
 
 }
