3f765839e858daf5076f3a64a91ecd50af297bc6
Николай Сандрюхин <n.sandryukhin@i-sys.ru>
2021-08-30 20:56:07 +0400

    task8 unit2

--- src/ru/isys/trainings/task8/Main.java
+++ src/ru/isys/trainings/task8/Main.java
@@ -1,9 +1,11 @@
 package ru.isys.trainings.task8;
 
 import ru.isys.trainings.task8.unit1.WomenNames;
+import ru.isys.trainings.task8.unit2.StringHelper;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,16 +13,71 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // unit1
         WomenNames wn = new WomenNames();
         String names = wn.getNames();
 
-        List<String> stream = Stream.of(names.split("\n"))
+        List<String> namesList = Stream.of(names.split("\n"))
                 .filter(item -> !item.equals("") && item.lastIndexOf("Имена для девочки на букву") == -1)
-                .map(item -> item.replaceAll(" .*",""))
+                .map(item -> item.replaceAll(" .*", ""))
                 .sorted(Comparator.reverseOrder())
                 .collect(Collectors.toList());
 
-        stream.forEach(System.out::println);
+//        namesList.forEach(System.out::println);
+
+        // unit2
+        List<String> vowels3 = namesList.stream()
+                .filter(item -> StringHelper.getVowelsCount(item) == 3)
+                .collect(Collectors.toList());
+
+//        vowels3.forEach(System.out::println);
+
+        List<String> vowels4 = namesList.stream()
+                .filter(item -> StringHelper.getVowelsCount(item) == 4)
+                .collect(Collectors.toList());
+
+//        vowels4.forEach(System.out::println);
+
+        List<String> vowels5 = namesList.stream()
+                .filter(item -> StringHelper.getVowelsCount(item) == 5)
+                .collect(Collectors.toList());
+
+//        vowels5.forEach(System.out::println);
+
+        String longName = namesList.stream()
+                .max(Comparator.comparing(String::length))
+                .orElse(null);
+
+//        System.out.println(longName);
+
+        String shortName = namesList.stream()
+                .min(Comparator.comparing(String::length))
+                .orElse(null);
+
+//        System.out.println(shortName);
+
+        String nameHas_YEEEEE = namesList.stream()
+                .filter(item -> item.indexOf("Е") == 0)
+                .findFirst()
+                .orElse(null);
+
+//        System.out.println(nameHas_YEEEEE);
+
+        boolean allHaveVowels = namesList.stream()
+                .allMatch(item -> StringHelper.getVowelsCount(item) > 0);
+
+//        System.out.println(allHaveVowels);
+
+        Map<String, Integer> namesMap = namesList.stream()
+                .distinct()
+                .collect(Collectors.toMap(item -> item, String::length));
+
+//        namesMap.forEach((key, value) -> {
+//            System.out.println(key + " => " + value);
+//        });
+
+
+
     }
 
 }
---
+++ src/ru/isys/trainings/task8/unit2/StringHelper.java
@@ -0,0 +1,23 @@
+package ru.isys.trainings.task8.unit2;
+
+import java.util.Locale;
+
+public class StringHelper {
+
+    public static int getVowelsCount(String str) {
+        char[] vowels = {'а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е'};
+        char[] strChars = str.toLowerCase(Locale.ROOT).toCharArray();
+
+        int counter = 0;
+        for (char strChar : strChars) {
+            for (char vowelChar : vowels) {
+                if (strChar == vowelChar) {
+                    counter++;
+                }
+            }
+        }
+
+        return counter;
+    }
+
+}
