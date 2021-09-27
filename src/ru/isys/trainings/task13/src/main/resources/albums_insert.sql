c7c417f3e50680daecda02b3f66d319153477a61
Николай Сандрюхин <n.sandryukhin@i-sys.ru>
2021-09-27 14:37:35 +0400

    task13 unit1

--- src/ru/isys/trainings/task13/src/main/resources/albums_insert.sql
+++ src/ru/isys/trainings/task13/src/main/resources/albums_insert.sql
@@ -1,5 +1,5 @@
-insert into albums (name, genre) values ('AlbumName1', 1);
-insert into albums (name, genre) values ('AlbumName2', 2);
-insert into albums (name, genre) values ('AlbumName3', 3);
-insert into albums (name, genre) values ('AlbumName4', 1);
-insert into albums (name, genre) values ('AlbumName5', 2);
+insert into albums (name, genre, singer) values ('AlbumName1', 1, 1);
+insert into albums (name, genre, singer) values ('AlbumName2', 2, 1);
+insert into albums (name, genre, singer) values ('AlbumName3', 3, 2);
+insert into albums (name, genre, singer) values ('AlbumName4', 1, 2);
+insert into albums (name, genre, singer) values ('AlbumName5', 2, 3);
---
+++ src/ru/isys/trainings/task13/src/main/resources/singers_insert.sql
@@ -0,0 +1,3 @@
+insert into singers (name) values ('Singer1');
+insert into singers (name) values ('Singer2');
+insert into singers (name) values ('Singer3');
